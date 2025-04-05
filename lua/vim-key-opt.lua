1cbeb016e7165cf1eb776ce1e74408bb48468506
magnusdice <joblicious13@gmail.com>
2025-04-05 15:41:54 +0800

    change keybind

--- lua/vim-key-opt.lua
+++ lua/vim-key-opt.lua
@@ -11,4 +11,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader><Tab>", vim.cmd.Ex)
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("i", "<C-.>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
