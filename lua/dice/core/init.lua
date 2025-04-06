b87326164c4046b2dea3c63fc16aa52275fb7f55
magnusdice <joblicious13@gmail.com>
2025-04-06 12:16:09 +0800

    change tab space

--- lua/dice/core/init.lua
+++ lua/dice/core/init.lua
@@ -1,7 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set scrolloff=8")
@@ -19,4 +19,3 @@ vim.keymap.set("n", "<leader><Tab>", vim.cmd.Ex)
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
-
--- lua/dice/plugins/lsp/lspconfig.lua
+++ lua/dice/plugins/lsp/lspconfig.lua
@@ -83,7 +83,6 @@ return {
 						"typescriptreact",
 						"javascriptreact",
 						"css",
-						"php",
 					},
 				})
 			end,
