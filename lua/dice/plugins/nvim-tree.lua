23228d74d14354f5b394cf4261da0466270ac3ac
magnusdice <joblicious13@gmail.com>
2025-04-06 16:20:24 +0800

    toggle nvim tree

--- lua/dice/plugins/nvim-tree.lua
+++ lua/dice/plugins/nvim-tree.lua
@@ -1,20 +1,24 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function ()
-    require("nvim-tree").setup({
- sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 30,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		})
+	end,
+	opts = {
+		modified = true,
+	},
 
-    })
-     end
+	vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle),
 }
