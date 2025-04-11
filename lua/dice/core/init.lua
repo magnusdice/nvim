25723eba015f99676332889854a3391eca4c37de
magnusdice <joblicious13@gmail.com>
2025-04-11 08:14:31 +0800

    added description

--- lua/dice/core/init.lua
+++ lua/dice/core/init.lua
@@ -21,23 +21,23 @@ vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("i", "<C-c>", "<Esc>") -- exit insert mode
-vim.keymap.set("i", "jj", "<Esc>") -- exit insert mode
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert 1" }) -- exit insert mode
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert 2" }) -- exit insert mode
 
 -- moving selections in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Moving selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Moving selection up" })
 
 --jumping stuff
-vim.keymap.set("n", "<C-D>", "<C-d>zz")
-vim.keymap.set("n", "<C-U>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "10jzz")
-vim.keymap.set("n", "<C-u>", "10kzz")
+vim.keymap.set("n", "<C-D>", "<C-d>zz", { desc = "Jump D" })
+vim.keymap.set("n", "<C-U>", "<C-u>zz", { desc = "Jump U" })
+vim.keymap.set("n", "<C-d>", "10jzz", { desc = "Jump 10 D" })
+vim.keymap.set("n", "<C-u>", "10kzz", { desc = "Jump 10 U" })
 
 -- split
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 -- vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>=", "<C-w>=", { desc = "Make splits equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- wrapping function
