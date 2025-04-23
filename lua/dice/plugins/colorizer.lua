0bb3e83fd67524ad2384fd6e15fea0541cee8112
magnusdice <joblicious13@gmail.com>
2025-04-23 08:35:24 +0800

    fix colorizer not displaying color

--- lua/dice/plugins/colorizer.lua
+++ lua/dice/plugins/colorizer.lua
@@ -1,3 +1,13 @@
 return {
 	"norcalli/nvim-colorizer.lua",
+	config = function()
+		require("colorizer").setup({
+			"css",
+			"javascript",
+			html = {
+				mode = "foreground",
+			},
+			css = { rgb_fn = true },
+		})
+	end,
 }
