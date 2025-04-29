09554d6d65393f21dc7e347e158dbe2d2516af84
magnusdice <joblicious13@gmail.com>
2025-04-29 16:06:28 +0800

    Added custom comments

--- lua/dice/plugins/comments.lua
+++ lua/dice/plugins/comments.lua
@@ -2,7 +2,20 @@ return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
+		keywords = {
+			-- //custom keyword that i will use
+			LOW = {
+				color = "hint",
+			},
+			MID = {
+				color = "warning",
+			},
+			HIGH = {
+				color = "error",
+			},
+		},
 		-- your configuration comes here
+		--
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 	},
