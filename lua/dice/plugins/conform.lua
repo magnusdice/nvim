f11c741476b3ba6d778f75b5c9d02650ce34c5bd
magnusdice <joblicious13@gmail.com>
2025-06-25 11:57:35 +0800

    remove gd script

--- lua/dice/plugins/conform.lua
+++ lua/dice/plugins/conform.lua
@@ -1,4 +1,3 @@
--- TODO: need to find a way to format php with html embed and js
 return {
 	"stevearc/conform.nvim",
 	config = function()
@@ -9,16 +8,10 @@ return {
 				php = { "easy-coding-standard" },
 				sql = { "sql-formatter" },
 				html = { "prettier" },
-				gd = { "gdtoolkit" },
-				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				-- rust = { "rustfmt", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
 				javascript = { "prettierd", stop_after_first = true },
 			},
 			format_on_save = {
-				-- These options will be passed to conform.format()
 				timeout_ms = 500,
 				lsp_format = "fallback",
 			},
